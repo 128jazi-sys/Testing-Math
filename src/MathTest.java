f4f20673ade6571bc55f6411c6299b861c7d4319
128jazi-sys <128jaziz@frhsd.com>
2026-09-18 12:15:24 -0400

    completed testOnePower

--- src/MathTest.java
+++ src/MathTest.java
@@ -111,7 +111,7 @@ public class MathTest {
     @ParameterizedTest
     @ValueSource(doubles = {1, 5.0, 3.7, -1, -4.0, -4.9})
     public void testOnePower(double base) {
-
+        assertEquals(base,Math.pow(base,1));
     }
 
 
